395c61f53f6919cb54ad42950df1f43025612401
Egor Kazachenko <eg.kazachenko@gmail.com>
2020-04-24 18:02:23 +0300

    -

--- lib/ios/AirMaps/AIRMapLocalTileOverlay.m
+++ lib/ios/AirMaps/AIRMapLocalTileOverlay.m
@@ -13,19 +13,4 @@
 
 @implementation AIRMapLocalTileOverlay
 
-
--(void)loadTileAtPath:(MKTileOverlayPath)path result:(void (^)(NSData *, NSError *))result {
-    NSMutableString *tileFilePath = [self.customPath mutableCopy];
-    
-    [tileFilePath replaceOccurrencesOfString: @"{x}" withString:[NSString stringWithFormat:@"%li", (long)path.x] options:0 range:NSMakeRange(0, tileFilePath.length)];
-    [tileFilePath replaceOccurrencesOfString:@"{y}" withString:[NSString stringWithFormat:@"%li", (long)path.y] options:0 range:NSMakeRange(0, tileFilePath.length)];
-    [tileFilePath replaceOccurrencesOfString:@"{z}" withString:[NSString stringWithFormat:@"%li", (long)path.z] options:0 range:NSMakeRange(0, tileFilePath.length)];
-    if ([[NSFileManager defaultManager] fileExistsAtPath:tileFilePath]) {
-        NSData* tile = [NSData dataWithContentsOfFile:tileFilePath];
-        result(tile,nil);
-    } else {
-        result(nil, nil);
-    }
-}
-
 @end
